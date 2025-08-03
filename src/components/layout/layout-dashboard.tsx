38c6c9471fd5d303923c7535577a32fcc07fda9d
zvn7 <aammaul7@gmail.com>
2025-08-03 10:51:19 +0700

    [delete] unusued import

--- src/components/layout/layout-dashboard.tsx
+++ src/components/layout/layout-dashboard.tsx
@@ -1,13 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { AppSidebar } from "@/components/layout/app-sidebar";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
