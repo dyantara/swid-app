1adb9db047136686b0fea8d9b02f583b8e8cacca
zvn7 <aammaul7@gmail.com>
2025-08-16 23:05:56 +0700

    tes la

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,19 +1,7 @@
-// lib/axios.ts
 import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-});
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token"); // atau sessionStorage/context
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    } else {
-        // pastikan header Authorization dihapus kalau token tidak ada
-        if (config.headers) delete config.headers.Authorization;
-    }
-    return config;
 });
 
 export default api;
